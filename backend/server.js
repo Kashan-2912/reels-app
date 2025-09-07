c130fab3779a7ecefe4a42ff976c617db79dd27f
Kashan-2912 <mkashan2912@gmail.com>
2025-09-07 14:49:23 +0500

    todo

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+// make models, controllers, routes, etc.
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
 });
 
 app.listen(3000, () => {
