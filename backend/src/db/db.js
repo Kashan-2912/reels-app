9b0aaa18559286fd17d2775b65ec059cfc9a24d7
Kashan-2912 <mkashan2912@gmail.com>
2025-09-08 20:39:07 +0500

    mongodb uri env

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDb() {
-    mongoose.connect('mongodb://localhost:27017/reel-app'
-    ).then(() => {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log('Connected to MongoDB');
     }).catch((err) => {
         console.error('Failed to connect to MongoDB', err);
